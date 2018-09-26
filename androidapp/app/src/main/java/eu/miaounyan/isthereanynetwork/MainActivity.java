4da4d71fab25259c038927c4dda925562a1e6747
Tordesillas <bubble83440@gmail.com>
2018-09-26 19:14:30 +0200

    Create scan progression

--- androidapp/app/src/main/java/eu/miaounyan/isthereanynetwork/MainActivity.java
+++ androidapp/app/src/main/java/eu/miaounyan/isthereanynetwork/MainActivity.java
@@ -1,6 +1,7 @@
 package eu.miaounyan.isthereanynetwork;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,22 +9,47 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private ProgressBar timeCountProgressBar;
+    private TextView timeCountTextView;
+    private final static int SCAN_TIME = 30;
+    private int remainingTime;
+    private CountDownTimer cdt;
+    private Button scanButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+        View scrollView = findViewById(R.id.main_page);
+        timeCountTextView = scrollView.findViewById(R.id.timeCount);
+        timeCountProgressBar = scrollView.findViewById(R.id.timeCount_progress_bar);
+
+        timeCountProgressBar.setMax(SCAN_TIME);
+        timeCountProgressBar.setProgress(remainingTime);
+        timeCountTextView.setText(SCAN_TIME + "");
+
+        scanButton = findViewById(R.id.scan_button);
+        scanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchTimer();
             }
         });
     }
@@ -48,5 +74,24 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void launchTimer() {
+        remainingTime = SCAN_TIME;
+        scanButton.setClickable(false);
+        cdt = new CountDownTimer(SCAN_TIME * 1000, 1000) {
+            public void onTick(long millisUntilFinished) {
+                remainingTime--;
+                timeCountTextView.setText(remainingTime + "");
+                timeCountProgressBar.setProgress(SCAN_TIME - remainingTime);
+            }
+
+            public void onFinish() {
+                timeCountTextView.setText("Done");
+                timeCountProgressBar.setProgress(0);
+                scanButton.setClickable(true);
+            }
+        };
+        cdt.start();
     }
 }
