6d4585da3ddb34900662151e3958d8eef263029e
piernov <piernov@piernov.org>
2018-10-23 13:06:27 +0200

    Server: CORS

--- server/app.js
+++ server/app.js
@@ -22,4 +22,13 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/networkstate', networkstateRouter);
 
+
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+}
+app.use(allowCrossDomain);
+
 module.exports = app;
