075154f50b8beb806ef1b0775258a425bdbe6c78
piernov <piernov@piernov.org>
2018-10-24 10:40:22 +0200

    Server: networkstate find by _id

--- server/routes/networkstate.js
+++ server/routes/networkstate.js
@@ -19,7 +19,6 @@ router.route('/')
   .post(function(req, res, next) {
     console.log(req.body);
     mongoose.model('networkstate').create(req.body, function (err, networkstate) {
-    console.log("miaou");
       if (err) {
         res.status(500);
         res.send({error: err});
@@ -30,6 +29,20 @@ router.route('/')
       }
     });
   });
-
+router.route('/:id')
+  .get(function(req, res, next) {
+    mongoose.model('networkstate').findOne({_id: mongoose.Types.ObjectId(req.params.id)}, function(err, networkstate) {
+      if (err) {
+        res.status(500);
+        res.send({error: err});
+        console.error(err);
+      } else if (!networkstate) {
+        res.status(404);
+        res.send("Not found: " + req.params.id);
+      } else {
+        res.send(networkstate);
+      }
+    });
+  });
 
 module.exports = router;
