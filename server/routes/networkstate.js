ec55a1dd311b432d1ce5c94f04615933ab31422c
piernov <piernov@piernov.org>
2018-11-07 09:52:51 +0100

    Server: add some filter (ss gt/lt, operator, protocol)

--- server/routes/networkstate.js
+++ server/routes/networkstate.js
@@ -11,6 +11,17 @@ router.route('/')
     if (req.query.to)
       researchparams.date = Object.assign(researchparams.date || {}, { '$lte': req.query.to });
 
+    if (req.query.operatorName)
+      researchparams.operatorName = req.query.operatorName;
+
+    if (req.query.networkProtocol)
+      researchparams.networkProtocol = req.query.networkProtocol;
+
+    if (req.query.signalStrengthLowerThan)
+      researchparams.signalStrength = Object.assign(researchparams.signalStrength || {}, { '$gte': req.query.signalStrengthLowerThan });
+    if (req.query.signalStrengthGreaterThan)
+      researchparams.signalStrength = Object.assign(researchparams.signalStrength || {}, { '$lte': req.query.signalStrengthGreaterThan });
+
     mongoose.model('networkstate').find(researchparams, function (err, networkstates) {
       if (err) {
         res.status(500);
