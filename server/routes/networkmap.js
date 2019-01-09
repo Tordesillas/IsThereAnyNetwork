536fed33056f2ef5b517915665fdfd30ebb5f199
piernov <piernov@piernov.org>
2019-01-09 12:08:21 +0100

    Server: networkmap guassian filter

--- server/routes/networkmap.js
+++ server/routes/networkmap.js
@@ -32,8 +32,11 @@ router.route('/')
         console.error(err);
       } else {
         let signalsArray = new Array(resolution * resolution);
-        for (i = 0; i < resolution*resolution; i++) {
-          signalsArray[i] = new Array(0);
+        for (i = 0; i < resolution; i++) {
+          signalsArray[i] = new Array(resolution);
+          for (j = 0; j < resolution; j++) {
+            signalsArray[i][j] = new Array(0);
+          }
         }
 
         networkstates.forEach(networkstate => {
@@ -41,14 +44,65 @@ router.route('/')
           const y = Math.floor(networkstate.latitude.map(minY, maxY, 0, resolution));
 
           if (networkstate.signalStrength < signalMax && networkstate.signalStrength > signalMin) { // filter out invalid values
-            signalsArray[y*resolution + x].push(networkstate.signalStrength);
+            signalsArray[y][x].push(Math.floor(Math.abs(networkstate.signalStrength))); // absolute value for subsequent operations
           }
 
         });
 
-        signalArray = signalsArray.map(t => t.length < 1 ? 0 : t.reduce((a,b) => a + b, 0) / t.length);
-        levelArray = signalArray.map(s => s == 0 ? 0 : Math.floor(s.map(signalMax, signalMin, 1, l)));
+        signalArray = signalsArray.map(row => {
+          return row.map(cell => cell.length < 1 ? 0 : cell.reduce((a,b) => a + b, 0) / cell.length);
+        });
 
+        kernel = [
+          [ 1, 2, 1 ],
+          [ 2, 4, 2 ],
+          [ 1, 2, 1 ]
+        ];
+
+        let filteredArray = new Array(resolution * resolution);
+        for (i = 0; i < resolution; i++) {
+          filteredArray[i] = new Array(resolution);
+          for (j = 0; j < resolution; j++) {
+            filteredArray[i][j] = 0;
+          }
+        }
+
+        for (i = 0; i < resolution; i++) {
+          for (j = 0; j < resolution; j++) {
+            let computedValues = [];
+            let computedFactors = [];
+
+            for (m = -1; m <= 1; m++) {
+              for (n = -1; n <= 1; n++) {
+                if (i + m < 0 || i + m >= resolution || j + n < 0 || j + n >= resolution) {
+                  continue; // ignore out of bounds
+                }
+                const value = signalArray[i + m][j + n];
+                if (value == 0) { // no or invalid value, ignore
+                  continue;
+                }
+                computedValues.push(kernel[m + 1][n + 1] * value);
+                computedFactors.push(kernel[m + 1][n + 1]);
+              }
+            }
+
+            if (computedValues.length < 1) { // no value could be computed
+              filteredArray[i][j] = 0;
+            } else {
+              const factor = computedFactors.reduce((a,b) => a + b, 0) / computedFactors.length;
+              const finalValue = computedValues.reduce((a,b) => a + b, 0) / factor;
+              console.log(finalValue, computedValues.length);
+              filteredArray[i][j] = finalValue;
+            }
+          }
+        }
+
+        console.log(filteredArray);
+        const flatArray = filteredArray.flatMap(t => t);
+        console.log(flatArray);
+
+        // Filters
+        levelArray = flatArray.map(s => s == 0 ? 0 : Math.floor(s.map(-signalMin, -signalMax, 1, l))); // we've taken absolute value before so use positive max and min
         res.send(levelArray);
       }
     });
