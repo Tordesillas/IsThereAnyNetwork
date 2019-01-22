5c4dd6f03fb4c428129d8e61fdb1a011e58a4ec0
piernov <piernov@piernov.org>
2019-01-23 00:09:49 +0100

    Android: adjust signal colors

--- androidapp/app/src/main/java/eu/miaounyan/isthereanynetwork/model/SignalStrength.java
+++ androidapp/app/src/main/java/eu/miaounyan/isthereanynetwork/model/SignalStrength.java
@@ -3,9 +3,9 @@ package eu.miaounyan.isthereanynetwork.model;
 public enum SignalStrength {
     TRANSPARENT(0x00FFFFFF),
     BAD(0x40FF0000),
-    BAD_AVERAGE(0x40FFFF00),
+    BAD_AVERAGE(0x40FF8000),
     AVERAGE(0x40FFFF00),
-    AVERAGE_GOOD(0x407FFF00),
+    AVERAGE_GOOD(0x4080FF00),
     GOOD(0x4000FF00);
 
     private final int color;
